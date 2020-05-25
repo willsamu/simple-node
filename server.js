f6a66e0efee9090a73fee15db9f49712298af988
willsamu <bok5@gmx.net>
2020-05-26 00:15:30 +0200

    Add enviroment var log

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 async function main() {
-  while(true) {
-    console.log('Containers rule!');
+  const simpleNodeEnv = process.env.SIMPLE_NODE_ENV;
+  while (true) {
+    console.log(`Containers rule! (in ${simpleNodeEnv})`);
     await sleep(5000);
   }
 }
